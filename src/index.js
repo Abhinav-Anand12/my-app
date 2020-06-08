3dd5cc8f601d881ad728156f8f1c66293ec4bdd3
Abhinav-Anand12 <abhinav.anand@314ecorp.com>
2020-06-08 13:49:50 +0530

    Clean up ignored files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Board } from './Board.jsx';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Board />,
   document.getElementById('root')
 );
 
